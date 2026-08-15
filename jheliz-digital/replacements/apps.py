7ed93e3681392b96218d23bbd6124dba88107d83
jianmolina646-lang <279170536+jianmolina646-lang@users.noreply.github.com>
2026-08-15 01:20:35 -0500

    Remove duplicated private project from public repository

--- jheliz-digital/replacements/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class ReplacementsConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "replacements"
-    verbose_name = "Reposiciones"
